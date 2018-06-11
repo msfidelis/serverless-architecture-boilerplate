7671fe8b8887af04b18fdad2ae29dd24d7c88568
Matheus Fidelis <msfidelis@outlook.com>
2018-06-11 09:53:56 -0300

    simple unit test

---
+++ test/shared/uuid.test.js
@@ -0,0 +1,16 @@
+const expect = require("chai").expect;
+const assert = require("chai").assert;
+
+const uuid = require('../../shared/lib/uuid');
+
+describe("#uuid library", () => {
+
+    it("#Generate a simple UUID", () => {
+        const id = uuid();
+        expect(id).to.be.a('string');
+    });
+
+    it("#Don't duplicate", () => {
+        expect(uuid()).to.not.equal(uuid());
+    });
+});
